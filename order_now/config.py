d89b60c1241a1ca4f2ed1436abd6daaec88ff3fe
BetaNeumann <brunomoretti100@gmail.com>
2024-08-26 17:16:43 -0400

    feat: added config.py file a source of truth

---
+++ order_now/config.py
@@ -0,0 +1,2 @@
+app_name = 'Order Now'
+version = '0.1.0'
--- order_now/controller/app.py
+++ order_now/controller/app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from .. import config
 
 app = FastAPI(
-    
+    title=config.app_name,
+    version=config.version,
+    default_response_class=ORJSONResponse
 )
